c47ecfd4f0a3edce4d95e771a28def37855b7c48
Avanthika <avanthikasaravanan123@gmail.com>
2026-05-15 20:03:51 -0700

    Updated the display of the current and next tasks on the timer

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ const taskTime = document.getElementById("taskTime");
 const addBtn = document.getElementById("addBtn");
 const taskList= document.getElementById("taskList");
 
+
 const timeDisplay = document.getElementById("timeDisplay");
 const startBtn = document.getElementById("startBtn");
 const pauseBtn = document.getElementById("pauseBtn");
 const resetBtn = document.getElementById("resetBtn");
+
+const currentTaskDisplay = document.getElementById("currentTask");
+const nextTaskDisplay = document.getElementById("nextTask");
+
+// buttons
 
 startBtn.addEventListener("click", startTimer);
 pauseBtn.addEventListener("click", pauseTimer);
@@ -40,8 +46,21 @@ function addTask () {
     // update 
     renderTasks(); 
 
-    //clear 
-    taskName.value = ""; 
+    // update current + next display
+    if (tasks.length > 0) {
+        currentTaskDisplay.textContent = `Current: ${tasks[0].name}`;
+    } else {
+        currentTaskDisplay.textContent = "Current: -";
+    }
+
+    if (tasks.length > 1) {
+        nextTaskDisplay.textContent = `Next: ${tasks[1].name}`;
+    } else {
+        nextTaskDisplay.textContent = "Next: -";
+    }
+
+    // clear inputs
+    taskName.value = "";
     taskTime.value = "";
 
 }
@@ -61,9 +80,28 @@ function renderTasks(){
 function startTimer() {
     if (timerInterval) return;
 
-if (currentTime <= 0 && task.length > 0) { 
+    if (tasks.length === 0) { 
+        alert("Add a task first"); 
+            return;
+    }
+
+if (currentTime <= 0) { 
     currentTime = tasks[0].time * 60;
 }
+
+updateDisplay (); 
+
+timerInterval = setInterval (() => {
+    if (currentTime <= 0) { 
+        clearInterval(timerInterval);
+        timerInterval = null; 
+        return;
+    }
+
+    currentTime--;
+
+    updateDisplay();
+}, 1000);
 
 timerInterval = setInterval(() => { 
     if (currentTime <=0) { 
